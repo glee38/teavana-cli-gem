4425993ea201c47f8760d8e2260dc338abf34c38
glee38 <glee3388@gmail.com>
2016-05-24 15:26:01 -0400

    refactor require_relative paths

--- lib/teavana_cli_gem.rb
+++ lib/teavana_cli_gem.rb
@@ -1,9 +1,10 @@
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
+require 'colorize'
 
 
-require_relative "./teavana_cli_gem/version"
-require_relative "./teavana_cli_gem/cli"
-require_relative "./teavana_cli_gem/teascraper"
-require_relative "./teavana_cli_gem/teas"
+require_relative "teavana_cli_gem/version"
+require_relative "teavana_cli_gem/cli"
+require_relative "teavana_cli_gem/teascraper"
+require_relative "teavana_cli_gem/teas"
